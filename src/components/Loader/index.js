dc6619fae327cf406c4108b0e9739585a713d5ac
Azfar Syal <azfarsyal@gmail.com>
2024-08-27 21:52:05 +0500

    Added Loadder

---
+++ src/components/Loader/index.js
@@ -0,0 +1,14 @@
+
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-[35svh]">
+    <div class="flex gap-2">
+      <div class="w-5 h-5 rounded-full animate-pulse bg-black"></div>
+      <div class="w-5 h-5 rounded-full animate-pulse bg-black"></div>
+      <div class="w-5 h-5 rounded-full animate-pulse bg-black"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
